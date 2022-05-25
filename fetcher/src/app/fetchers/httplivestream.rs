a6cfe179a559de5248f2bea2c52f067966360dd3
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2022-05-25 19:55:12 +0200

    fetch playlist and emit segment durations

--- fetcher/src/app/fetchers/httplivestream.rs
+++ fetcher/src/app/fetchers/httplivestream.rs
@@ -1,8 +1,11 @@
+use std::str::FromStr;
 use std::time::Duration;
 
-use anyhow::Result;
+use anyhow::{anyhow, bail, Context, Result};
 use async_stream::try_stream;
-use reqwest::Url;
+use hls_m3u8::MediaPlaylist;
+use reqwest::header::CONTENT_TYPE;
+use reqwest::{StatusCode, Url};
 use tokio_stream::Stream;
 
 pub struct HttpLiveStreamingFetcher {
@@ -18,49 +21,47 @@ impl HttpLiveStreamingFetcher {
         }
     }
 
-    /// Receive the next message published on a subscribed channel, waiting if
-    /// necessary.
-    ///
-    /// `None` indicates the subscription has been terminated.
-    pub async fn next_segment(&mut self) -> Result<Option<u32>> {
-        tokio::time::sleep(Duration::from_secs(1)).await;
-        // match self.client.connection.read_frame().await? {
-        //     Some(mframe) => {
-        //         debug!(?mframe);
+    async fn fetch_playlist(&self) -> Result<MediaPlaylist> {
+        let response = self.client.get(self.source.clone()).send().await?;
 
-        //         match mframe {
-        //             Frame::Array(ref frame) => match frame.as_slice() {
-        //                 [message, channel, content] if *message == "message" => Ok(Some(Message {
-        //                     channel: channel.to_string(),
-        //                     content: Bytes::from(content.to_string()),
-        //                 })),
-        //                 _ => Err(mframe.to_error()),
-        //             },
-        //             frame => Err(frame.to_error()),
-        //         }
-        //     }
-        //     None => Ok(None),
-        // }
-        Ok(Some(20))
+        if response.status() != StatusCode::OK {
+            bail!(
+                "Failed to get data from source: {} {}",
+                response.status(),
+                response.text().await?
+            );
+        }
+
+        let content_type = response
+            .headers()
+            .get(CONTENT_TYPE)
+            .ok_or_else(|| anyhow!("Missing content-type header"))?;
+
+        let content_type = content_type.to_str()?;
+
+        if content_type != "application/vnd.apple.mpegurl; charset=UTF-8" {
+            bail!("Invalid content-type header: {}", content_type);
+        }
+
+        MediaPlaylist::from_str(response.text().await?.as_ref())
+            .map_err(|e| anyhow!("Failed to parse playlist: {:#}", e))
     }
 
-    /// Convert the subscriber into a `Stream` yielding new messages published
-    /// on subscribed channels.
-    ///
-    /// `Subscriber` does not implement stream itself as doing so with safe code
-    /// is non trivial. The usage of async/await would require a manual Stream
-    /// implementation to use `unsafe` code. Instead, a conversion function is
-    /// provided and the returned stream is implemented with the help of the
-    /// `async-stream` crate.
-    pub fn fetch(mut self) -> impl Stream<Item = Result<u32>> {
+    pub fn fetch(self) -> impl Stream<Item = Result<u64>> {
         log::trace!(target: "HttpLiveStreamingFetcher", "Fetching source={}", &self.source);
+
         // Uses the `try_stream` macro from the `async-stream` crate. Generators
         // are not stable in Rust. The crate uses a macro to simulate generators
         // on top of async/await. There are limitations, so read the
         // documentation there.
         try_stream! {
-            while let Some(segment) = self.next_segment().await? {
-                yield segment
+            loop {
+                let playlist = self.fetch_playlist().await?;
+                for (_, segment) in &playlist.segments {
+                    yield segment.duration.duration().as_secs()
+                }
+
+                tokio::time::sleep(playlist.duration() / 2).await;
             }
         }
     }
