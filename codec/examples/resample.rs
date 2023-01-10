60fd9735613d5ca56ff08d74466289d6f892a1e0
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2023-01-10 19:35:55 +0700

    elaborate resample example

--- codec/examples/resample.rs
+++ codec/examples/resample.rs
@@ -1,13 +1,21 @@
 use std::io::Write;
 
-use codec::{CodecParams, SampleFormat};
+use codec::{CodecParams, Decoder, SampleFormat};
 
 fn main() -> anyhow::Result<()> {
     let input = std::env::args().nth(1).expect("Expects audio file");
 
     let io = std::io::BufReader::new(std::fs::File::open(input)?);
 
-    let resampled = codec::resample(io, CodecParams::new(22050, SampleFormat::S16, 1))?;
+    let decoder = Decoder::try_from(io)?.resample(CodecParams::new(22050, SampleFormat::S16, 1));
+
+    let mut resampled: Vec<u8> = vec![];
+
+    for frame in decoder {
+        for plane in frame?.planes().iter() {
+            resampled.extend_from_slice(plane.data());
+        }
+    }
 
     std::io::stdout().write_all(&resampled)?;
     std::io::stdout().flush()?;
