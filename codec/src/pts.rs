8f26b252ee483678364f812e77e42db77b782765
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2023-03-27 22:52:43 +0700

    Pts is calculated from the base duration

--- codec/src/pts.rs
+++ codec/src/pts.rs
@@ -1,83 +1,56 @@
 use std::time::Duration;
 
-use ac_ffmpeg::codec::audio::AudioFrame;
 use ac_ffmpeg::time::Timestamp;
 
-use crate::FrameDuration;
-
 pub struct Pts {
-    pts: Timestamp,
     duration: Duration,
-}
-
-impl From<&AudioFrame> for Pts {
-    fn from(frame: &AudioFrame) -> Self {
-        let pts = Timestamp::new(0, frame.time_base());
-        let duration = frame.duration();
-        Self { pts, duration }
-    }
+    counter: u32,
 }
 
 impl Pts {
+    #[must_use]
+    pub fn new(samples_per_frame: u32, sample_rate: u32) -> Pts {
+        let duration = f64::from(samples_per_frame) / f64::from(sample_rate);
+        let duration = Duration::from_secs_f64(duration);
+        let counter = 0;
+
+        Self { duration, counter }
+    }
+
     #[allow(clippy::should_implement_trait)]
     pub fn next(&mut self) -> Timestamp {
-        let pts = self.pts;
-        self.pts += self.duration;
-        pts
+        let timestamp = self.duration * self.counter;
+        self.counter += 1;
+        Timestamp::from_micros(timestamp.as_micros().try_into().unwrap_or_default())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use ac_ffmpeg::codec::audio::{AudioFrameMut, ChannelLayout};
-
-    use crate::{Pts, SampleFormat};
+    use crate::Pts;
 
     #[test]
     fn test_1024_of_44_100() {
-        let frame = AudioFrameMut::silence(
-            ChannelLayout::from_channels(1).unwrap().as_ref(),
-            SampleFormat::Flt.into(),
-            44_100,
-            1024,
-        )
-        .freeze();
+        let mut pts = Pts::new(1_024, 44_100);
 
-        let mut pts = Pts::from(&frame);
-        assert_eq!(pts.next().as_nanos(), Some(0));
-        assert_eq!(pts.next().as_nanos(), Some(23_219_000));
-        assert_eq!(pts.next().as_nanos(), Some(46_438_000));
+        assert_eq!(pts.next().as_micros(), Some(0));
+        assert_eq!(pts.next().as_micros(), Some(23_219));
+        assert_eq!(pts.next().as_micros(), Some(46_439));
     }
 
     #[test]
     fn test_4_of_4() {
-        let frame = AudioFrameMut::silence(
-            ChannelLayout::from_channels(1).unwrap().as_ref(),
-            SampleFormat::Flt.into(),
-            4,
-            4,
-        )
-        .freeze();
-
-        let mut pts = Pts::from(&frame);
-        assert_eq!(pts.next().as_nanos(), Some(0));
-        assert_eq!(pts.next().as_nanos(), Some(1_000_000_000));
-        assert_eq!(pts.next().as_nanos(), Some(2_000_000_000));
+        let mut pts = Pts::new(4, 4);
+        assert_eq!(pts.next().as_micros(), Some(0));
+        assert_eq!(pts.next().as_micros(), Some(1_000_000));
+        assert_eq!(pts.next().as_micros(), Some(2_000_000));
     }
 
     #[test]
-    fn test_960_of_48_000() {
-        let frame = AudioFrameMut::silence(
-            ChannelLayout::from_channels(2).unwrap().as_ref(),
-            SampleFormat::Flt.into(),
-            48_000,
-            960,
-        )
-        .freeze();
-
-        let mut pts = Pts::from(&frame);
-        assert_eq!(pts.next().as_nanos(), Some(0));
-        assert_eq!(pts.next().as_nanos(), Some(20_000_000));
-        assert_eq!(pts.next().as_nanos(), Some(40_000_000));
+    fn test_2048_of_48_000() {
+        let mut pts = Pts::new(2_048, 48_000);
+        assert_eq!(pts.next().as_micros(), Some(0));
+        assert_eq!(pts.next().as_micros(), Some(42_666));
+        assert_eq!(pts.next().as_micros(), Some(85_333));
     }
 }
